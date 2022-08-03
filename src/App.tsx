e73c460eb3f379ed8989db16f9dfc8997bb0c9b5
Seochangh2 <nomm89@naver.com>
2022-08-03 16:06:53 +0900

    redux없는 react컴포넌트 구조 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
-
+import styles from "./App.module.css";
+import axios from "axios";
+import AddNumberRoot from "./components/AddNumberRoot";
+import DisplayNumberRoot from "./components/DisplayNumberRoot";
 function App() {
+  const getClick = async () => {
+    try {
+      axios.get("http://alexjun12.loca.lt/bike/unlockRequest");
+    } catch (error) {
+      console.log("s;kdjal");
+    }
+  };
+  const postClick = async (post: string) => {
+    try {
+      axios.post("http://alexjun12.loca.lt/bike/lockBike", {
+        lockbike: post,
+      });
+      console.log(post);
+    } catch (e) {
+      console.log("sdad");
+    }
+  };
   return (
-    <div className="App">
-      <h2>shdhs</h2>
+    <div className={styles.App}>
+      <div className={styles.JJY}>
+        <button className={styles.btn} onClick={() => getClick()}>
+          겟
+        </button>
+        <button className={styles.btn} onClick={() => postClick("false")}>
+          폴스맨
+        </button>
+        <button className={styles.btn} onClick={() => postClick("true")}>
+          트루맨
+        </button>
+      </div>
+      <div>
+        <h1>Root</h1>
+        <AddNumberRoot></AddNumberRoot>
+        <DisplayNumberRoot></DisplayNumberRoot>
+      </div>
     </div>
   );
 }
